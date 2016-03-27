c0ee5ffe190481aab9715648122afffd463859c0
Jonathan Barket <jbarket@sleepunit.com>
2016-03-26 23:26:54 -0500

    Added smooth scroll, slightly adjusted responsive timeline

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 import TypeRotate from './modules/TypeRotate';
 import Timeline from './modules/Timeline';
+import smoothScroll from 'smooth-scroll';
 
 TypeRotate.init();
+smoothScroll.init();
 
 let timeline = new Timeline();
